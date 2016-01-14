9819ae81cc316f712a9fc75f5ea0060de73648ba
Agrim Asthana <agrimasthana@gmail.com>
2016-01-14 14:40:58 -0600

    live reload enabled with the help of browserify TBD

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var flatten = require('gulp-flatten');
-
+var uglify = require('gulp-uglify');
+var browsersync = require('browser-sync').create();
+//var reload = browsersync.reload;
 var PATH = {
     src: './**/*.ts'
 };
@@ -18,30 +20,30 @@ gulp.task('clean',function(done){
 
 gulp.task('ts2js',function(){
    var tsResult = tsProject.src().pipe(ts(tsProject));
-    return tsResult.js.pipe(flatten()).pipe(gulp.dest('compiled'));
+    return tsResult.js.pipe(flatten()).pipe(uglify()).pipe(gulp.dest('compiled'));
 });
 
-//gulp.task('ts2js',function(done){
-//   var typescript = require('gulp-typescript');
-//   var tscConfig = require('./tsconfig.json');
-//    gulp.src(PATH.src).pipe(typescript(tscConfig.compilerOptions));
-//        //.pipe(gulp.dest('built/local'));
-//});
+gulp.task('reload',function(){
+   browsersync.reload();
+});
 
-gulp.task('serve', ['ts2js'], function () {
-    var http = require('http');
-    var connect = require('connect');
-    var serveStatic = require('serve-static');
-    var open = require('open');
+gulp.task('serve',['ts2js'], function () {
+    //var http = require('http');
+    //var connect = require('connect');
+    //var serveStatic = require('serve-static');
+    //var open = require('open');
 
-    var port = 9000, app;
-
+    //var port = 9000, app;
+    browsersync.init({server:{baseDir:"./"}});
+    //livereload.listen();
     gulp.watch(PATH.src, ['ts2js']);
+    gulp.watch('./**/*.html',['reload']);
+    gulp.watch('./**/*.sass',['reload']);
 
-    app = connect().use(serveStatic(__dirname));
-    http.createServer(app).listen(port, function () {
-        open('http://localhost:' + port);
-    });
+    //app = connect().use(serveStatic(__dirname));
+    //http.createServer(app).listen(port, function () {
+    //    open('http://localhost:' + port);
+    //});
 });
 
 gulp.task('default',['serve']);
